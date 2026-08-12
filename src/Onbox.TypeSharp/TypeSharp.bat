dfb76bc1be8ba480b236d39192f39f0012163f79
Thiago Almeida <t.almeidaeng@gmail.com>
2026-08-12 10:05:45 +0930

    Removed deprecated CSharp project

--- src/Onbox.TypeSharp/TypeSharp.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-"bin\Debug\net5.0\win-x86\TypeSharp.exe" --version
-"bin\Debug\net5.0\win-x86\TypeSharp.exe" --source "..\..\samples\SampleModels\bin\Debug\netstandard2.0" --file-filter "SampleModels.dll" --destination "..\..\samples\SampleModels\Typescript"
